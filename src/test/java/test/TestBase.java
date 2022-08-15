0609b876f2a6cd53047fdbf74ace99999a4c9946
Lena Natykan Bethge <elena.natykan@gmail.com>
2022-08-16 00:13:55 +0200

    Initial commit

---
+++ src/test/java/test/TestBase.java
@@ -0,0 +1,21 @@
+package test;
+
+import fw.ApplicationManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+
+    protected ApplicationManager app = new ApplicationManager();
+
+    @BeforeMethod
+    public void setUp() {
+        app.init();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        app.stop();
+    }
+
+}
